132d4f1a1727e9497150d1e3441678afb1055c6b
crimson-carnival <etherealcarnival@gmail.com>
2020-09-05 14:32:21 +0530

    Tester job - blank

--- src/main/groovy/dsl.groovy
+++ src/main/groovy/dsl.groovy
@@ -1,3 +1,10 @@
-multibranchPipelineJob("tester") {
-
+multibranchPipelineJob('tester') {
+	branchSources {
+		git {
+			id('123456789') // IMPORTANT: use a constant and unique identifier
+			remote('http://4c510e99f1ce/ethereal-carnival/building-a-multibranch-pipeline-project.git')
+			credentialsId('ede7c40b-876a-4160-82fb-9a77a712949c')
+			includes('develop|prod')
+		}
+	}
 }
