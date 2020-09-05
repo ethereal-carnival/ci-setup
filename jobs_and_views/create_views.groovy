83d37efa24012ffc52a13a3183396641862c9136
crimson-carnival <etherealcarnival@gmail.com>
2020-09-05 15:03:11 +0530

    Added list view

--- jobs_and_views/create_views.groovy
+++ jobs_and_views/create_views.groovy
@@ -2,6 +2,23 @@ buildMonitorView('Wall') {
     jobs {
         regex('.*')
     }
-
     recurse(true)
+}
+
+listView('List') {
+    description('All jobs')
+    filterBuildQueue()
+    filterExecutors()
+    jobFilters {
+        all()
+    }
+    columns {
+        status()
+        weather()
+        name()
+        lastSuccess()
+        lastFailure()
+        lastDuration()
+        buildButton()
+    }
 }
