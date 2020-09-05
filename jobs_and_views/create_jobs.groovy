1542d88c27cc3097fcd245682cf03a3de3b460eb
crimson-carnival <etherealcarnival@gmail.com>
2020-09-05 18:23:53 +0530

    Removed master from getting built

--- jobs_and_views/create_jobs.groovy
+++ jobs_and_views/create_jobs.groovy
@@ -4,7 +4,7 @@ multibranchPipelineJob('Test Pipeline') {
             id('1')
             remote('http://gitlab/ethereal-carnival/building-a-multibranch-pipeline-project.git')
             credentialsId('ede7c40b-876a-4160-82fb-9a77a712949c')
-            includes('develop master prod')
+            includes('develop prod')
         }
     }
 }
